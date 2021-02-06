cd6d3bfeb358499aea3eabe191e9ccb194451aeb
David Morales <dmoralessal87@gmail.com>
2021-02-06 19:44:25 +0100

    Agregados eventos a los campos, si se dejan vacio se les añade una clase con Tailwind

--- JavaScript/Enviar Emails/js/app.js
+++ JavaScript/Enviar Emails/js/app.js
@@ -1,6 +1,11 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
 
+// Variables campos formularios
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 eventListeners();
 
 /**
@@ -10,6 +15,11 @@ function eventListeners() {
 
     // Cuando el documento esta listo iniciamos nuestra APP
     document.addEventListener('DOMContentLoades', iniciarApp);
+
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -24,4 +34,22 @@ function iniciarApp() {
     btnEnviar.disabled = ture;
     // Cambiamos el cursor al apsar por encima y la opacidad
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+/**
+ * Valida los campos del formulario en tiempo real
+ */
+function validarFormulario(e) {
+
+    const elemento = e.target;
+
+    // Acceder a los que el usuario escribe en el input
+    console.log(elemento.value);
+
+    if(elemento.value.lenght > 0) {
+        
+    } else {
+        // Añadimos estilos si el campo esta vacío con Tailwind
+        elemento.classList.add('border', 'border-red-500');
+    }
 }
