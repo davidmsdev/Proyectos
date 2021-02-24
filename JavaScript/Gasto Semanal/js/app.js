e33690384f195d53fd32a606086754bd43fdffb7
David Morales <dmoralessal87@gmail.com>
2021-02-24 15:52:31 +0100

    Mostrando los gastos en el HTML

--- JavaScript/Gasto Semanal/js/app.js
+++ JavaScript/Gasto Semanal/js/app.js
@@ -26,7 +26,6 @@ class Presupuesto {
     nuevoGasto(gasto) {
         // Agregamos el nuevo gasto despues de hacer una copia del actual
         this.gastos = [...this.gastos, gasto];
-        console.log(this.gastos);
     }
 }
 
@@ -47,7 +46,7 @@ class UI {
         if(tipo === 'error') {
             divMensaje.classList.add('alert-danger');
         } else {
-            divMensaje.classList.add('alert-succes');
+            divMensaje.classList.add('alert-success');
         }
 
         // Mesnaje de error
@@ -60,6 +59,42 @@ class UI {
         setTimeout(() => {
             divMensaje.remove();
         }, 3000);
+    }
+
+    agregarGastoListado(gastos) {
+        
+        this.limpiarHTML();
+        
+        // Iterar sobre los gastos
+        gastos.forEach( gasto => {
+            
+            const { cantidad, nombre, id } = gasto;
+
+            // Crear un li
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id; // Agrega el atributo data-id
+            console.log(nuevoGasto);
+
+            // Agregar HTML del gasto
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> ${cantidad}</span>`;
+
+            // Botón para borrar el gasto
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = 'Borrar &times;';
+            nuevoGasto.appendChild(btnBorrar);
+
+            // Inerta el HTML
+            gastoListado.appendChild(nuevoGasto);
+        })
+    }
+
+    limpiarHTML() {
+        // Iteramos mientras gastoListado tenga elementos HTML y los va borrando
+        while(gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
     }
 }
 
@@ -105,6 +140,10 @@ function agregarGasto(e) {
     presupuesto.nuevoGasto(gasto);
 
     ui.imprimirAlerta('Gasto agregado');
+
+    // Imprimir los gastos, los extraemos de presupuesto
+    const { gastos } = presupuesto;
+    ui.agregarGastoListado(gastos);
 
     formulario.reset();
 }
