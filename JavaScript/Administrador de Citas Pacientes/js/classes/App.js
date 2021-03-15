ef26bc6a991fe0c693e9fee40625d37079eb2f86
David Morales <dmoralessal87@gmail.com>
2021-03-15 16:19:50 +0100

    Crear BD

--- JavaScript/Administrador de Citas Pacientes/js/classes/App.js
+++ JavaScript/Administrador de Citas Pacientes/js/classes/App.js
@@ -13,6 +13,7 @@ class App {
 
     constructor() {
         this.initApp();
+        this.crearDB();
     }
 
     initApp() {
@@ -25,6 +26,47 @@ class App {
 
         // Formulario para nuevas citas
         formulario.addEventListener('submit', nuevaCita);
+    }
+
+    crearDB() {
+
+        // Crear BD en versión 1.0
+        const crearDB = window.indexedDB.open('citas', 1);
+
+        // Si hay un error
+        crearDB.onerror = function() {
+            console.log('Hubo un error');
+        }
+
+        // Si todo esta OK
+        crearDB.onsuccess = function() {
+            console.log('DB creada');
+
+            const DB = crearDB.result;
+
+            console.log(DB);
+        }
+
+        // Definir el Schema
+        crearDB.onupgradeneeded = function(e) {
+            const db = e.target.result;
+
+            const objectStore = db.createObjectStore('citas', {
+                keyPath: 'id',
+                autoIncrement: true
+            });
+
+            // Definir todas las columnas
+            objectStore.createIndex('mascota', 'mascota', { unique: false });
+            objectStore.createIndex('propietario', 'propietario', { unique: false });
+            objectStore.createIndex('telefono', 'telefono', { unique: false });
+            objectStore.createIndex('fecha', 'fecha', { unique: false });
+            objectStore.createIndex('hora', 'hora', { unique: false });
+            objectStore.createIndex('sintomas', 'sintomas', { unique: false });
+            objectStore.createIndex('id', 'id', { unique: true });
+
+            console.log('DB creada y lista');
+        }
     }
 }
 
