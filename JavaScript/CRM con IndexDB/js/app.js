e0e108a7dd0638436d76860969c21e413032676c
David Morales <dmoralessal87@gmail.com>
2021-04-14 15:39:09 +0200

    Eliminando un cliente

--- JavaScript/CRM con IndexDB/js/app.js
+++ JavaScript/CRM con IndexDB/js/app.js
@@ -1,6 +1,7 @@
 (function(){
 
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -8,7 +9,35 @@
         if(window.indexedDB.open('crm', 1)) {
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click', eliminarRegistro);
     });
+
+    function eliminarRegistro(e) {
+        if(e.target.classList.contains('eliminar')) {
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('Deseas elminar el cliente?');
+
+            if(confirmar) {
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function() {
+                    // Navegamos hasta el TR que contiene el cliente para eliminarlo
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function() {
+                    console.log('Hubo un error');
+                }
+            }
+
+            console.log(idEliminar);
+        }
+    }
 
     // Crea la base de datos
     function crearDB() {
@@ -76,7 +105,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;
