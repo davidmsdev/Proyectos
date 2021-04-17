a6cc0635e80cc2ea2d7778e0d8ee9d02077220ab
David Morales <dmoralessal87@gmail.com>
2021-04-17 21:43:46 +0200

    Mostrando Spinner de carga

--- JavaScript/API - Obtener Clima/js/app.js
+++ JavaScript/API - Obtener Clima/js/app.js
@@ -51,6 +51,9 @@ function consultarAPI(ciudad, pais) {
     const appId = '1647db81fd95a0f7c0adc751be5dbe53';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
+    // Mostramos Spinner de carga
+    spinner();
+
     fetch(url)
         .then( respuesta => respuesta.json())
         .then( datos => {
@@ -108,4 +111,30 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function spinner() {
+
+    limpiarHTML();
+
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
+
 }
