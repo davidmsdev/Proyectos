3bdb4c3a5e7be6bda7aaa7d69ea63e3f1d325c81
David Morales <dmoralessal87@gmail.com>
2021-04-17 19:08:23 +0200

    Mostrando la temperatura actual

--- JavaScript/API - Obtener Clima/js/app.js
+++ JavaScript/API - Obtener Clima/js/app.js
@@ -54,8 +54,41 @@ function consultarAPI(ciudad, pais) {
     fetch(url)
         .then( respuesta => respuesta.json())
         .then( datos => {
+
+            limpiarHTML();
+
             if(datos.cod === "404") {
                 mostrarError('Ciudad no encontrada');
+                return;
             }
+
+            // Imprime la respuesta en el HTML
+            mostrarClima(datos);
         })
+}
+
+function mostrarClima(datos) {
+    const { main: { temp, temp_max, temp_min }} = datos;
+
+    const centigrados = kelvinACentrigrados(temp);
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(actual);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+// Convertimos los grados a centigrados
+const kelvinACentrigrados = grados => parseInt(grados - 273.15);
+
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
