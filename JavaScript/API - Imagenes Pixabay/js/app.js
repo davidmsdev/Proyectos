f576febf414bebb76603ce6b918a43ea8c032a80
David Morales <dmoralessal87@gmail.com>
2021-04-18 02:00:19 +0200

    Mostrando las imágenes

--- JavaScript/API - Imagenes Pixabay/js/app.js
+++ JavaScript/API - Imagenes Pixabay/js/app.js
@@ -14,7 +14,57 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un término de búsqueda');
         return;
     }
+
+    buscarImagenes(terminoBusqueda);
 }
+
+function buscarImagenes(termino) {
+
+    console.log(termino);
+
+    const key = '21209552-4c9217a3fdc7952a196683822';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=50`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            mostrarImagenes(resultado.hits)
+        });
+}
+
+function mostrarImagenes(imagenes) {
+    
+    while(resultado.firstChild) {
+        // Eliminamos cada primer elemento hasta que no quede nnguno
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Accedemos a cada imágen
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                    <p class="font-bold"> ${likes}<span class="font-light"> Me Gusta</span></p>
+                    <p class="font-bold"> ${views}<span class="font-light"> Veces vista</span></p>
+
+                    <a 
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
+                    > 
+                        Ver imagen 
+                    </a>
+                </div>
+                </div>
+            </div>   
+        `;
+    })
+}
+
 
 function mostrarAlerta(mensaje) {
 
