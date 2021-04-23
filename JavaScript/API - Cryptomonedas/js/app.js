2986b3f8bad214f125090693bf01497436e2835d
David Morales <dmoralessal87@gmail.com>
2021-04-24 00:36:52 +0200

    Mostrando toda la info de la API

--- JavaScript/API - Cryptomonedas/js/app.js
+++ JavaScript/API - Cryptomonedas/js/app.js
@@ -1,6 +1,7 @@
 const criptosSelect = document.querySelector('#criptomonedas');
 const formulario = document.querySelector('#formulario');
 const monedaSelect = document.querySelector('#moneda');
+const resultado = document.querySelector('#resultado');
 
 // Objeto de busqueda, ponemos los names de los campos select como claves para poder igualarlos
 const objBusqueda = {
@@ -94,5 +95,37 @@ function consultarAPI() {
 }
 
 function mostrarCotizacionHTML(cotizacion) {
+
+    limpiarHTML();
     
+    // Extraemos los datos que queremos 
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El Precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `El Precio más alto ha sido: <span>${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `El Precio más bajo ha sido: <span>${LOWDAY}</span>`;
+
+    const ultimasHoras = document.createElement('p');
+    ultimasHoras.innerHTML = `Variacion últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const ultimaActualizacion = document.createElement('p');
+    ultimaActualizacion.innerHTML = `Ultima actualización: <span>${LASTUPDATE}</span>`;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
