9df65cd94cc51fbca429cd4734abd9fe2e5bdc87
David Morales <dmoralessal87@gmail.com>
2021-08-19 14:54:51 +0200

    Eliminadoes console log

--- JavaScript/CalculadoraIncrementoPorcentual/sw.js
+++ JavaScript/CalculadoraIncrementoPorcentual/sw.js
@@ -6,11 +6,8 @@ self.addEventListener('install', e => {
 // Activar el Service Worker
 self.addEventListener('activate', e => {
     console.log('Service Worker activado');
-
-    console.log(e);
 });
 
 // Evento FETCH para descargar archivos estáticos
 self.addEventListener('fetch', e => {
-    console.log('Fetch... ', e);
 });
