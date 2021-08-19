b6cee32953e39d17e62826d9a73fede89e1586bd
David Morales <dmoralessal87@gmail.com>
2021-08-19 12:35:35 +0200

    Eliminado cache

--- JavaScript/CalculadoraIncrementoPorcentual/sw.js
+++ JavaScript/CalculadoraIncrementoPorcentual/sw.js
@@ -1,26 +1,6 @@
-const cacheName = 'cip-v1';
-
-// Cachear archivos
-const archives = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/app.js',
-    '/js/registerWorker.js',
-];
-
 // Cuando se instala el service worker
 self.addEventListener('install', e => {
     console.log('Instalado el Service Worker');
-
-    // Cargamso el sevice worker y esperamos a que se descarguen todos los archivos que queremos
-    e.waitUntil(
-        caches.open(cacheName)
-            .then( cache => {
-                console.log('Cacheando...');
-                cache.addAll(archives);
-            })
-    )
 });
 
 // Activar el Service Worker
@@ -33,10 +13,4 @@ self.addEventListener('activate', e => {
 // Evento FETCH para descargar archivos estáticos
 self.addEventListener('fetch', e => {
     console.log('Fetch... ', e);
-
-    e.respondWith(
-        caches
-            .match(e.request)
-            .then(cacheResponse => (cacheResponse ? cacheResponse : fetch(e.request)))
-    )
 });
