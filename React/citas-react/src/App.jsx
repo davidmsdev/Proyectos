f8ee50aa34fc1713e346d19445ebe0fd76a3f154
David Morales <dmoralessal87@gmail.com>
2022-03-27 12:13:16 +0200

    Finalizado

--- React/citas-react/src/App.jsx
+++ React/citas-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Form from "./components/Form"
 import Header from "./components/Header"
 import PatientList from "./components/PatientList"
@@ -7,6 +7,20 @@ function App() {
 
   const [patients, setPatients] = useState([])
   const [patient, setPatient] = useState({})
+
+  useEffect(() => {
+    const getLocalStorage = () => {
+      // Convertimos el STRING en un Array/objeto
+      const patientsLS = JSON.parse(localStorage.getItem(`pacientes`)) ?? []
+      setPatients(patientsLS)
+    }
+    getLocalStorage()
+  }, [])
+
+  useEffect(() => {
+    console.log(`Componente listo o cambió pacientes`)
+    localStorage.setItem(`pacientes`, JSON.stringify(patients))
+  }, [patients])
 
   const deletePatient = id => {
     console.log(`Eliminando paciente`)
